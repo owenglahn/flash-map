d34b7251dac20bcd0574b6fd62cff7be51bbfaf9
Owen Glahn <owen.glahn@mail.mcgill.ca>
2026-04-07 16:49:51 -0400

    Fix user model name

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,7 +1,7 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
 
-const User = sequelize.define("User", {
+const User = sequelize.define("user", {
   username: {
     type: DataTypes.STRING,
     allowNull: false,
